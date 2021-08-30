8c166cc99b78ac74d5a3bfcb67f4f0caba44622a
noelmiranda <noelmiranda19@gmail.com>
2021-08-30 21:41:07 +0200

    button-pagination

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -12,11 +12,19 @@ pageNumber.push(i)
           <ul className="pagination justify-content-center">
               {
                   pageNumber.map(number => (
-                      <li key={number} className="page-item">
-                      <a onClick={() => paginate(number)} href="!#" className="page-link">
-                          {number}
-                      </a>
-                      </li>
+                      <button
+                      type="button"
+                      className="btn btn-outline-secondary me-md-2"
+                      key={number}
+                      onClick={() => paginate(number)}
+                      >                        {number}  
+                      </button>
+
+                    //   <li key={number} className="page-item">
+                    //   <a onClick={() => paginate(number)} href="default.html" className="page-link">
+                    //       {number}
+                    //   </a>
+                    //   </li>
                   ))
               }
           </ul>
